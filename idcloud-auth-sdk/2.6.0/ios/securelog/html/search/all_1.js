6c437953df2f43df8206099ee7f367495bb662a5
LijingZhang8 <lijing.zhang@thalesgroup.com>
2024-03-15 15:54:30 +0800

    added api doc for 2.6.0

---
+++ idcloud-auth-sdk/2.6.0/ios/securelog/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['fileid_0',['fileID',['../interface_secure_log_config_components.html#aa57225555a747f896e1058c0b2048e64',1,'SecureLogConfigComponents::fileID()'],['../interface_secure_log_config.html#a3b37d675479afaf4c5a50d0dfaae7871',1,'SecureLogConfig::fileID()']]],
+  ['files_1',['files',['../protocol_secure_log-p.html#abb999f1dc5d428ad1e5eb145420a2a23',1,'SecureLog-p']]]
+];
---
+++ idcloud-auth-sdk/2.6.0/ios/securelog/html/search/files_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['securelog_2eh_0',['SecureLog.h',['../_secure_log_8h.html',1,'']]],
+  ['securelogapi_2eh_1',['SecureLogAPI.h',['../_secure_log_a_p_i_8h.html',1,'']]],
+  ['securelogconfig_2eh_2',['SecureLogConfig.h',['../_secure_log_config_8h.html',1,'']]]
+];
