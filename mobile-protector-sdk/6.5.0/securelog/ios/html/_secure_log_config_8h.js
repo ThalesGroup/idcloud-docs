690a9a02d1784dc37a12acfb4a0d7662430dc969
LijingZhang8 <lijing.zhang@thalesgroup.com>
2024-04-02 09:28:55 +0800

    add api doc 6.5.0 for protector sdk

---
+++ mobile-protector-sdk/6.5.0/securelog/ios/html/_secure_log_config_8h.js
@@ -0,0 +1,6 @@
+var _secure_log_config_8h =
+[
+    [ "SecureLogConfigComponents", "interface_secure_log_config_components.html", "interface_secure_log_config_components" ],
+    [ "SecureLogConfig", "interface_secure_log_config.html", "interface_secure_log_config" ],
+    [ "NS_ENUM", "_secure_log_config_8h.html#ad894a6332fe64503a1c7e7e1530aba8d", null ]
+];
