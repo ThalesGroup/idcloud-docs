f35a54dc8274bf3abbbcb8613274da4c7b9d74ee
Mervyn Ong <mervyn.ong@cplcloud.com>
2025-03-24 11:16:12 +0800

    idcloud-fido: update docs for fido 2.8.0 and 3.0.0

---
+++ idcloud-auth-sdk/3.0.0/ios/securelog/html/search/all_7.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['securelog_2dp_0',['SecureLog-p',['../protocol_secure_log-p.html',1,'']]],
+  ['securelog_2eh_1',['SecureLog.h',['../_secure_log_8h.html',1,'']]],
+  ['securelogapi_2eh_2',['SecureLogAPI.h',['../_secure_log_a_p_i_8h.html',1,'']]],
+  ['securelogconfig_3',['SecureLogConfig',['../interface_secure_log_config.html',1,'']]],
+  ['securelogconfig_2eh_4',['SecureLogConfig.h',['../_secure_log_config_8h.html',1,'']]],
+  ['securelogconfigcomponents_5',['SecureLogConfigComponents',['../interface_secure_log_config_components.html',1,'']]],
+  ['setlevel_3a_6',['setLevel:',['../protocol_secure_log-p.html#a822e2c0202c3b25c980eaa5452265c4b',1,'SecureLog-p']]]
+];
---
+++ idcloud-auth-sdk/3.0.0/ios/securelog/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['files_0',['files',['../protocol_secure_log-p.html#abb999f1dc5d428ad1e5eb145420a2a23',1,'SecureLog-p']]]
+];
