b1533e646f94d82f532598ec24d7545c4c886a55
LijingZhang8 <lijing.zhang@thalesgroup.com>
2025-08-29 11:10:32 +0800

    update ios fido2 v4.0.0 apidoc

---
+++ fido2-sdk/4.0.0/ios/securelog/html/interface_secure_log_config.js
@@ -0,0 +1,12 @@
+var interface_secure_log_config =
+[
+    [ "initWithConfigComponentsBuilder:", "interface_secure_log_config.html#ae757fd8f1c9e610a70c835e1292e4ffd", null ],
+    [ "NS_UNAVAILABLE", "interface_secure_log_config.html#a960974e512a2be73aa1f1ad979fbca90", null ],
+    [ "directory", "interface_secure_log_config.html#af08174e71434c0ba87c82002537e1886", null ],
+    [ "fileID", "interface_secure_log_config.html#a3b37d675479afaf4c5a50d0dfaae7871", null ],
+    [ "logLevel", "interface_secure_log_config.html#a387a65e49022ad4ec81b4f79f11c7660", null ],
+    [ "publicKeyExponent", "interface_secure_log_config.html#aa37950021965a3ed92ca52e8021678cc", null ],
+    [ "publicKeyModulus", "interface_secure_log_config.html#a137702a8e1fed9d99c9c66a2a90fc15a", null ],
+    [ "rollingFileMaxCount", "interface_secure_log_config.html#a745d0170aebb4e4fcc67cbaaaf5c99db", null ],
+    [ "rollingSizeInKB", "interface_secure_log_config.html#aa8970be499b6a3fcd41a0e071d266b29", null ]
+];
---
+++ fido2-sdk/4.0.0/ios/securelog/html/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ns_5fenum_0',['NS_ENUM',['../_secure_log_config_8h.html#ad894a6332fe64503a1c7e7e1530aba8d',1,'SecureLogConfig.h']]],
+  ['ns_5funavailable_1',['NS_UNAVAILABLE',['../protocol_secure_log-p.html#a7538dbc9709490f4d73167c7b04316b4',1,'SecureLog-p::NS_UNAVAILABLE()'],['../interface_secure_log_config.html#a960974e512a2be73aa1f1ad979fbca90',1,'SecureLogConfig::NS_UNAVAILABLE()']]]
+];
