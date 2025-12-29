ae2554e7f97f7d7b6f1b0c442fe0b3e800f7d563
Himanshu Goel <himanshu.goel@thalesgroup.com>
2025-12-29 14:38:41 +0800

    android: Update securelog doc to 2.0.0

---
+++ mobile-protector-sdk/6.7.0/securelog/android/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.thalesgroup.gemalto.securelog","l":"SecureLogConfig.Builder"},{"p":"com.thalesgroup.gemalto.securelog","l":"SecureLog"},{"p":"com.thalesgroup.gemalto.securelog","l":"SecureLogConfig"},{"p":"com.thalesgroup.gemalto.securelog","l":"SecureLogLevel"}];updateSearchResults();
